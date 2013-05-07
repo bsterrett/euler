f4257482e37ca3077d010a5d1e0e5161ee915182
Ben Sterrett <bs11868@gmail.com>
2013-05-07 12:51:41 -0400

    working on 243

--- solutions/prob243.py
+++ solutions/prob243.py
@@ -1,55 +1,61 @@
 #!python
-
+from itertools import combinations
 from fractions import Fraction
 
 def get_primes(upper_bound):
-	prime_dict = dict()
-	prime_dict[0] = False
-	prime_dict[1] = False
-	for i in xrange(2,upper_bound+1): prime_dict[i] = True
-	for i in xrange(2,upper_bound+1):
-		factors = range(i,upper_bound+1,i)
-		for f in factors[1:]:
-			prime_dict[f] = False
-	primes = []
-	for i in xrange(2,upper_bound):
-		if prime_dict[i]: primes.append(i)
-	return primes
+    prime_dict = dict()
+    prime_dict[0] = False
+    prime_dict[1] = False
+    for i in range(2,upper_bound+1): prime_dict[i] = True
+    for i in range(2,upper_bound+1):
+        factors = range(i,upper_bound+1,i)
+        for f in factors[1:]:
+            prime_dict[f] = False
+    primes = []
+    for i in range(2,upper_bound):
+        if prime_dict[i]: primes.append(i)
+    prime_dict = dict()
+    return primes
 
-def get_resilience(denom, primes_list):
-	prime_factors = []
-	for prime in primes_list:
-		if prime < denom and denom%prime == 0:
-			prime_factors.append(prime)
-	all_multiples = []
-	for factor in prime_factors:
-		for multiple in range(factor,denom,factor):
-			if all_multiples.count(multiple) == 0: all_multiples.append(multiple)	
-	return Fraction(denom-1-len(all_multiples),denom-1)
+def get_resilience(denom):
+    def get_prime_factors(number):
+        prime_factors = []
+        primes = get_primes(number+1)
+        for prime in primes:
+            if number%prime == 0: prime_factors.append(prime)
+        return prime_factors
+    def count_multiples(upper_bound, factors):
+        sum = 0
+        switch = 1
+        for length in range(1,len(factors)+1):
+            for factor_group in combinations(factors,length):
+                #print "Factor group:", factor_group
+                product = 1
+                for factor in factor_group: product *= factor
+                multiples = upper_bound/product
+                sum += switch * multiples
+            switch *= -1            
+        return sum
+    prime_factors = get_prime_factors(denom)
+    multiples = count_multiples(denom, prime_factors)
+    return Fraction(denom-multiples,denom-1)
 	
 if __name__ == '__main__':
-
 	last_resilience = best_resilience = Fraction(1,1)
 	best_denominator = -1
 	
-	primes = get_primes(35)
-	for i in range(2,30):
-		#get_resilience(i,primes)
-		print ""
-		#print ""
 	
 	denom = 2
 	prime_counter = 1
 	prime_upper_bound = 100
 	primes = get_primes(prime_upper_bound)
 	while(best_resilience >= Fraction(15499,94744)):
-		new_resilience = get_resilience(denom,primes)
+		new_resilience = get_resilience(denom)
 		if new_resilience < best_resilience:
-			print "Denom:", denom, "  Resilience:", new_resilience
+			print "Denom:", denom, "  Resilience:", float(new_resilience)
 			best_resilience = new_resilience
 			best_denominator = denom
 		denom *= primes[prime_counter]
 		primes = get_primes(denom)
 		prime_counter += 1
-		print "Finished with", prime_counter, "-", denom
 	print "Best denominator:", best_denominator
--- solutions/ptest.py
+++ solutions/ptest.py
@@ -1,4 +1,54 @@
 #!python
 import pelib
+import time
+from itertools import combinations
+from fractions import Fraction
 
-print pelib.check_primality_dyn(5,200)
+def get_primes(upper_bound):
+	prime_dict = dict()
+	prime_dict[0] = False
+	prime_dict[1] = False
+	for i in range(2,upper_bound+1): prime_dict[i] = True
+	for i in range(2,upper_bound+1):
+		factors = range(i,upper_bound+1,i)
+		for f in factors[1:]:
+			prime_dict[f] = False
+	primes = []
+	for i in range(2,upper_bound):
+		if prime_dict[i]: primes.append(i)
+	return primes
+    
+def get_resilience_backup(denom):
+    resil = 1
+    for num in range(2,denom):
+        if denom == Fraction(num, denom).denominator:
+            resil += 1
+    return Fraction(resil,denom-1)
+
+def get_resilience(denom):
+    def get_prime_factors(number):
+        prime_factors = []
+        primes = get_primes(number+1)
+        for prime in primes:
+            if number%prime == 0: prime_factors.append(prime)
+        return prime_factors
+    def count_multiples(upper_bound, factors):
+        sum = 0
+        switch = 1
+        for length in range(1,len(factors)+1):
+            for factor_group in combinations(factors,length):
+                product = 1
+                for factor in factor_group: product *= factor
+                multiples = upper_bound/product
+                sum += switch * multiples
+            switch *= -1            
+        return sum
+            
+            
+            
+    prime_factors = get_prime_factors(denom)
+    #print prime_factors
+    multiples = count_multiples(denom, prime_factors)
+    return Fraction(denom-multiples,denom-1)
+    
+for i in range(2,50): print i, "-", get_resilience_backup(i), "  ", get_resilience(i)
