f8430899bb7e63fcb5def3ed2fdca3f5dc6e1e57
Ben <bs11868@gmail.com>
2013-05-06 23:04:08 -0300

    Update prob243.py

--- solutions/prob243.py
+++ solutions/prob243.py
@@ -3,7 +3,7 @@
 from fractions import Fraction
 
 def get_primes(upper_bound):
-  prime_dict = dict()
+	prime_dict = dict()
 	prime_dict[0] = False
 	prime_dict[1] = False
 	for i in xrange(2,upper_bound+1): prime_dict[i] = True
@@ -12,44 +12,44 @@ def get_primes(upper_bound):
 		for f in factors[1:]:
 			prime_dict[f] = False
 	primes = []
-	for i in xrange(3,upper_bound):
+	for i in xrange(2,upper_bound):
 		if prime_dict[i]: primes.append(i)
 	return primes
 
-def get_resilience(denom):
-	resil = 1
-	if denom%2 == 0:
-		for num in xrange(3,denom,2):
-			try:
-				if Fraction(num,denom).denominator == denom:	resil += 1
-			except TypeError:
-				print "Oops, type error!"
-	else:
-		for num in xrange(2,denom):
-			try:
-				if Fraction(num,denom).denominator == denom:	resil += 1
-			except TypeError:
-				print "Oops, type error!"
-	return Fraction(resil,denom-1)
+def get_resilience(denom, primes_list):
+	prime_factors = []
+	for prime in primes_list:
+		if prime < denom and denom%prime == 0:
+			prime_factors.append(prime)
+	all_multiples = []
+	for factor in prime_factors:
+		for multiple in range(factor,denom,factor):
+			if all_multiples.count(multiple) == 0: all_multiples.append(multiple)	
+	return Fraction(denom-1-len(all_multiples),denom-1)
 	
 if __name__ == '__main__':
 
 	last_resilience = best_resilience = Fraction(1,1)
 	best_denominator = -1
 	
-	denom = 
-	prime_counter = 5
+	primes = get_primes(35)
+	for i in range(2,30):
+		#get_resilience(i,primes)
+		print ""
+		#print ""
+	
+	denom = 2
+	prime_counter = 1
 	prime_upper_bound = 100
 	primes = get_primes(prime_upper_bound)
 	while(best_resilience >= Fraction(15499,94744)):
-		new_resilience = get_resilience(denom)
+		new_resilience = get_resilience(denom,primes)
 		if new_resilience < best_resilience:
 			print "Denom:", denom, "  Resilience:", new_resilience
 			best_resilience = new_resilience
 			best_denominator = denom
 		denom *= primes[prime_counter]
-		if prime_counter + 1 == len(primes):
-			prime_upper_bound *= 30
-			primes = get_primes(prime_upper_bound)
+		primes = get_primes(denom)
 		prime_counter += 1
+		print "Finished with", prime_counter, "-", denom
 	print "Best denominator:", best_denominator
