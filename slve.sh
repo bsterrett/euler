cc5f9825301bd670eef4721251f13afe9ea4a218
Ben <ben@registria.com>
2014-04-04 16:55:15 -0700

    cleaning up whitespace on slve script

--- slve.sh
+++ slve.sh
@@ -2,7 +2,7 @@
 
 if [ -x python ]; then
 	python=python
-elif [ -x /bin/python ]; then 
+elif [ -x /bin/python ]; then
 	python='/bin/python'
 elif [ -x /usr/bin/python ]; then
 	python='/usr/bin/python'
@@ -53,27 +53,27 @@ if [[ "all" -ne "$1" && "$1" -gt 0 ]] ; then
 	fi
 elif [[ "all" -eq "$1" ]] ; then
 	#execute all solutions
-    echo "Executing all solutions!"
-    for number in {1..500}
-    do
-        solution=prob${number}.py
-        if [ -x $solution ]; then
-            printf "Working on solution %d   ---   " $number
-            start_time=`date +%s`
+  echo "Executing all solutions!"
+  for number in {1..500}
+  do
+    solution=prob${number}.py
+    if [ -x $solution ]; then
+      printf "Working on solution %d   ---   " $number
+      start_time=`date +%s`
 			output=$($python $solution)
 			exit_status=$?
-            echo $output
-			output_target_value=`echo "$output" | sed 's|.*:\s*\(-*\d*\b\)|\1|1'`			
+      echo $output
+			output_target_value=`echo "$output" | sed 's|.*:\s*\(-*\d*\b\)|\1|1'`
 			end_time=`date +%s` ; time_elapsed=`expr $end_time - $start_time`
 			printf "%s,%d,%s\n" $number ${output_target_value} $time_elapsed >> $results_file
 			if [[ $time_elapsed -gt 20 ]] ; then 
 				printf "%s,%d,%s\n" $number ${output_target_value} $time_elapsed >> $slow_solutions_file
 			fi
 			printf "Execution time was %d seconds\n\n" $time_elapsed
-        fi
-    done
-    exit 0
+    fi
+  done
+  exit 0
 else
-    echo "Didn't understand input."
-    exit 1
+  echo "Didn't understand input."
+  exit 1
 fi
